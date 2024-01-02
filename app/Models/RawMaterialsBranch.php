72daa804eee9e13a9ae8ae987ebe46425f094e19
Marlou Pepito <marloupepito07@gmail.com>
2024-01-02 19:59:51 +0800

    dal;kdkad;la

--- app/Models/RawMaterialsBranch.php
+++ app/Models/RawMaterialsBranch.php
@@ -4,26 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasOne; // Fix the typo here
 
 class RawMaterialsBranch extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-      'raw_materials_id',
-      'branch_id',
-      'quantity',
-      'bind',
-      'warning',
-   ];
-   public function raw_materials(): HasOne
-   {
-       return $this->hasOne(RawMaterial::class,'id','raw_materials_id');
-   }
-   public function branch(): HasOne
-   {
-       return $this->hasOne(Branch::class,'id','branch_id');
-   }
-   
+        'raw_materials_id',
+        'branch_id',
+        'quantity',
+        'bind',
+        'warning',
+    ];
+
+    public function raw_materials(): HasOne
+    {
+        return $this->hasOne(RawMaterial::class, 'id', 'raw_materials_id');
+    }
+
+    public function branch(): HasOne
+    {
+        return $this->hasOne(Branch::class, 'id', 'branch_id');
+    }
 }
